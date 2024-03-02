5a457187c9cec09a8a5953845a0e184b0fff23dd
It Soda <nikshchegilskiy@gmail.com>
2024-03-02 19:59:20 +0300

    fix| car

--- carsalon/urls.py
+++ carsalon/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("cars.urls")),
+    path('', include("cars.urls")),
 ]
 
 urlpatterns += doc_url
